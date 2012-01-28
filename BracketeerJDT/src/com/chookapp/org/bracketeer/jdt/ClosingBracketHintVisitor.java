a054fe1601dfbf0da73141215814a420397545ce
Gil Barash <chookapp@yahoo.com>
2012-01-28 12:09:18 +0200

    Added hints for "break"

--- BracketeerJDT/src/com/chookapp/org/bracketeer/jdt/ClosingBracketHintVisitor.java
+++ BracketeerJDT/src/com/chookapp/org/bracketeer/jdt/ClosingBracketHintVisitor.java
@@ -10,17 +10,23 @@
  *******************************************************************************/
 package com.chookapp.org.bracketeer.jdt;
 
+import java.util.EmptyStackException;
 import java.util.Iterator;
+import java.util.Stack;
 
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.BreakStatement;
+import org.eclipse.jdt.core.dom.ContinueStatement;
+import org.eclipse.jdt.core.dom.DoStatement;
 import org.eclipse.jdt.core.dom.EnhancedForStatement;
 import org.eclipse.jdt.core.dom.ForStatement;
 import org.eclipse.jdt.core.dom.IfStatement;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.SwitchCase;
 import org.eclipse.jdt.core.dom.SwitchStatement;
 import org.eclipse.jdt.core.dom.SynchronizedStatement;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
@@ -34,10 +40,38 @@ import com.chookapp.org.bracketeer.common.IHintConfiguration;
 
 public class ClosingBracketHintVisitor extends ASTVisitor
 {
+    
+    class ScopeInfo
+    {
+        public String _str;
+        public int _offset;
+        public Statement _statement;
+        
+        public ScopeInfo(String str, int offset, Statement statement)
+        {
+            _str = str;
+            _offset = offset;
+            _statement = statement;
+        }        
+    }
+
+    public class ScopeTraceException extends Exception
+    {   
+        private static final long serialVersionUID = 6297837237586982280L;
+          
+        public ScopeTraceException(String message)
+        {
+            super(message);
+        }
+    }
+
+        
+    
     Boolean _cancelProcessing;
     IBracketeerProcessingContainer _container;
     IHintConfiguration _hintConf;
     IDocument _doc;
+    Stack<ScopeInfo> _scopeStack;
     
     public ClosingBracketHintVisitor(IBracketeerProcessingContainer container,
                                      IDocument doc, Boolean cancelProcessing, 
@@ -46,7 +80,160 @@ public class ClosingBracketHintVisitor extends ASTVisitor
         _cancelProcessing = cancelProcessing;
         _container = container;
         _doc = doc;
-        _hintConf = hintConf;        
+        _hintConf = hintConf;
+        
+        _scopeStack = new Stack<ClosingBracketHintVisitor.ScopeInfo>();
+    }
+    
+    private void removeFromStack(Statement node)
+    {
+        try
+        {
+            ScopeInfo scope;
+            if( node instanceof SwitchStatement )
+            {
+                scope = _scopeStack.peek();
+                if(scope._statement instanceof SwitchCase)
+                {
+                    _scopeStack.pop();
+                }
+            }
+
+            scope = _scopeStack.pop();
+            if(!scope._statement.getClass().equals(node.getClass()))
+            {
+                if(Activator.DEBUG)
+                {
+                    Activator.log("Lost track of scope. Expected:" + scope._statement + 
+                                  " but was:" + node);
+                }
+            }
+        }       
+        catch(EmptyStackException e)
+        {
+            if(Activator.DEBUG)
+                Activator.log(e);
+        }
+    }
+    
+    @Override
+    public boolean visit(SwitchCase node)
+    {
+        try
+        {
+            ScopeInfo scope = _scopeStack.peek();
+            if( !(scope._statement instanceof SwitchStatement) )
+            {
+                if(!(scope._statement instanceof SwitchCase))
+                {
+                    throw new ScopeTraceException("Lost track of stack (in case), found:" + scope._statement);
+                }
+
+                _scopeStack.pop();
+                scope = _scopeStack.peek();
+            }
+
+            if( !(scope._statement instanceof SwitchStatement) )
+            {
+                throw new ScopeTraceException("Lost track of stack (in case2), found:" + scope._statement);
+            }
+
+            String hint = "";
+            if( node.isDefault() )
+            {
+                hint = "default";
+            }
+            else
+            {
+                hint = "case: " + node.getExpression();
+            }
+
+            int startLoc = node.getStartPosition();
+            _scopeStack.push(new ScopeInfo(scope._str + " - " + hint, startLoc, node)); 
+        }
+        catch(ScopeTraceException e)
+        {
+            if(Activator.DEBUG)
+                Activator.log(e);
+        }
+        catch(EmptyStackException e)
+        {
+            if(Activator.DEBUG)
+                Activator.log(e);
+        }
+        
+        return shouldContinue();
+    }
+    
+    @Override
+    public boolean visit(DoStatement node)
+    {
+        String hint = GetNodeText(node.getExpression());
+        int startLoc = node.getStartPosition();
+        hint = "do_while( "+hint+" )";
+        _scopeStack.push(new ScopeInfo(hint, startLoc, node));
+        return shouldContinue();    
+    }    
+    
+    @Override
+    public void endVisit(DoStatement node)
+    {
+        removeFromStack(node);
+        super.endVisit(node);
+    }
+    
+    @Override
+    public boolean visit(BreakStatement node)
+    {
+        return visitBreak(node);
+    }  
+
+    @Override
+    public boolean visit(ContinueStatement node)
+    {
+        return visitBreak(node);
+    }
+    
+    
+    private boolean visitBreak(Statement node)
+    {
+        try
+        {
+            if(_scopeStack.isEmpty())
+                throw new ScopeTraceException("break without scope: " + node);
+
+            ScopeInfo scope = _scopeStack.peek();
+
+            if(node instanceof BreakStatement)
+            {
+                // ignoring break with labels on them
+                if( ((BreakStatement)node).getLabel() != null )
+                    return shouldContinue();
+            }
+
+            String hintType;
+            if( scope._statement instanceof ForStatement )
+                hintType = "break-for";
+            else if( scope._statement instanceof EnhancedForStatement )
+                hintType = "break-foreach";
+            else if( scope._statement instanceof WhileStatement )
+                hintType = "break-while";
+            else if( scope._statement instanceof DoStatement )
+                hintType = "break-do";
+            else if( scope._statement instanceof SwitchCase )
+                hintType = "break-case";
+            else
+                throw new ScopeTraceException("Unexpect scope ("+scope._statement+") on break/continue:" + node);
+
+            int endLoc = node.getStartPosition() + node.getLength() - 1; 
+            _container.add(new Hint(hintType, scope._offset, endLoc, scope._str));
+        }
+        catch(ScopeTraceException e)
+        {
+            if(Activator.DEBUG)
+                Activator.log(e);
+        }       
+        return shouldContinue();
     }
     
     @Override
@@ -62,19 +249,22 @@ public class ClosingBracketHintVisitor extends ASTVisitor
     @Override
     public boolean visit(MethodDeclaration node)
     {
+        // starting a function empties the stack... (which should already be empty on good flow)
+        _scopeStack.clear();
+        
         StringBuffer hint = new StringBuffer();
         hint.append(node.getName().getIdentifier());
         /* TODO: specific params: exclude function parameters (show only the name) */
-        hint.append('(');
+        hint.append("( ");
         for (@SuppressWarnings("rawtypes")
         Iterator iterator = node.parameters().iterator(); iterator.hasNext();)
         {
             SingleVariableDeclaration param = (SingleVariableDeclaration) iterator.next();
             hint.append(param.getName());
             if( iterator.hasNext() )
-                hint.append(',');
+                hint.append(", ");
         }
-        hint.append(')');
+        hint.append(" )");
         int startLoc = node.getName().getStartPosition();
         int endLoc = node.getStartPosition() + node.getLength() - 1;
         _container.add(new Hint("function", startLoc, endLoc, hint.toString())); //$NON-NLS-1$
@@ -86,21 +276,39 @@ public class ClosingBracketHintVisitor extends ASTVisitor
     {
         /* TODO: specific params: show also initializer && increment expressions */
         String hint = GetNodeText(node.getExpression());
+        hint = "for( "+hint+" )";
         int startLoc = node.getStartPosition();
-        int endLoc = startLoc + node.getLength() - 1;        
-        _container.add(new Hint("for", startLoc, endLoc, "for("+hint+")")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        int endLoc = startLoc + node.getLength() - 1;
+        _scopeStack.push(new ScopeInfo(hint, startLoc, node));
+        _container.add(new Hint("for", startLoc, endLoc, hint)); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         return shouldContinue();
     }
 
+    @Override
+    public void endVisit(ForStatement node)
+    {
+        removeFromStack(node);
+        super.endVisit(node);
+    }
+    
     @Override
     public boolean visit(EnhancedForStatement node)
     {
         /* TODO: specific params: put 2 checkboxes: the var name & the collection */
         String hint = GetNodeText(node.getExpression());
         int startLoc = node.getStartPosition();
-        int endLoc = startLoc + node.getLength() - 1;        
-        _container.add(new Hint("foreach", startLoc, endLoc, "foreach("+hint+")")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        int endLoc = startLoc + node.getLength() - 1;
+        hint = "foreach( "+hint+" )";
+        _scopeStack.push(new ScopeInfo(hint, startLoc, node));
+        _container.add(new Hint("foreach", startLoc, endLoc, hint)); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         return shouldContinue();
+    }
+    
+    @Override
+    public void endVisit(EnhancedForStatement node)
+    {
+        removeFromStack(node);
+        super.endVisit(node);
     }
     
     @Override
@@ -108,9 +316,18 @@ public class ClosingBracketHintVisitor extends ASTVisitor
     {
         String hint = GetNodeText(node.getExpression());
         int startLoc = node.getStartPosition();
-        int endLoc = startLoc + node.getLength() - 1;        
-        _container.add(new Hint("switch", startLoc, endLoc, "switch("+hint+")")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        int endLoc = startLoc + node.getLength() - 1;
+        hint = "switch( "+hint+" )";
+        _scopeStack.push(new ScopeInfo(hint, startLoc, node));
+        _container.add(new Hint("switch", startLoc, endLoc, hint)); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         return shouldContinue();
+    }
+    
+    @Override
+    public void endVisit(SwitchStatement node)
+    {
+        removeFromStack(node);
+        super.endVisit(node);
     }
     
     @Override
@@ -118,9 +335,18 @@ public class ClosingBracketHintVisitor extends ASTVisitor
     {
         String hint = GetNodeText(node.getExpression());
         int startLoc = node.getStartPosition();
-        int endLoc = startLoc + node.getLength() - 1;        
-        _container.add(new Hint("while", startLoc, endLoc, "while("+hint+")")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        int endLoc = startLoc + node.getLength() - 1;
+        hint = "while( "+hint+" )";
+        _scopeStack.push(new ScopeInfo(hint, startLoc, node));
+        _container.add(new Hint("while", startLoc, endLoc, hint)); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         return shouldContinue();
+    }
+    
+    @Override
+    public void endVisit(WhileStatement node)
+    {
+        removeFromStack(node);
+        super.endVisit(node);
     }
     
     @Override
@@ -129,7 +355,7 @@ public class ClosingBracketHintVisitor extends ASTVisitor
         String hint = GetNodeText(node.getExpression());
         int startLoc = node.getStartPosition();
         int endLoc = startLoc + node.getLength() - 1;        
-        _container.add(new Hint("synchronized", startLoc, endLoc, "synchronized("+hint+")")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        _container.add(new Hint("synchronized", startLoc, endLoc, "synchronized( "+hint+" )")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         return shouldContinue();
     }
 
@@ -171,7 +397,7 @@ public class ClosingBracketHintVisitor extends ASTVisitor
                 if( endLoc == -1 )
                     endLoc = thenStmt.getStartPosition() + thenStmt.getLength()-1;
                 int startLoc = node.getStartPosition();
-                _container.add(new Hint("if", startLoc, endLoc, "if("+hint+")")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                _container.add(new Hint("if", startLoc, endLoc, "if( "+hint+" )")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
             }
             
             if( elseStmt != null && !(elseStmt instanceof IfStatement) && 
@@ -179,7 +405,7 @@ public class ClosingBracketHintVisitor extends ASTVisitor
             {
                 endLoc = elseStmt.getStartPosition() + elseStmt.getLength()-1;
                 int startLoc = elseStmt.getStartPosition();
-                _container.add(new Hint("if", startLoc, endLoc, "else_of_if("+hint+")")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                _container.add(new Hint("if", startLoc, endLoc, "else_of_if( "+hint+" )")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
             }
             
         }
